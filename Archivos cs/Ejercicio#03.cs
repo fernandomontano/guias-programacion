81623f89fdafabd362936a4e8e11985f1f6b34cc
Fernando Montano <fernando.montano41@gmail.com>
2021-01-29 21:46:15 -0600

    Subir guias correspondientes

---
+++ Archivos cs/Ejercicio#03.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ecuacionEjercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Título de consola
+                Console.Title = "Volumen de una esfera";
+
+                //Descripción del proyecto
+                Console.WriteLine("Este programa calculará el volumen de una esfera");
+
+                //Declaración de variables
+                double radio, volumen;
+
+                //Solicitud de datos
+                Console.WriteLine("Introduce el valor del radio:");
+                radio = double.Parse(Console.ReadLine());
+
+                //Aplicación de formula
+                volumen = (4 * Math.PI * Math.Pow(radio, 3)) / 3;
+
+                //Math.Round para cortar el resultado a 2 decimales
+                double volumenDecimal = Math.Round(volumen, 2);
+
+                //Impresión de resultados
+                Console.WriteLine($"El volumen de una esfera con radio {radio}cm es: {volumenDecimal}cm^3");
+
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("--Tipo de dato ingresado no válido--");
+            }
+        }
+    }
+}
---
+++ Proyectos/Ejercicio#04/distanciaEjercicio4/distanciaEjercicio4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace distanciaEjercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Título de la consola
+                Console.Title = "Distancia entre 2 puntos";
+
+                //Descripción del programa
+                Console.WriteLine("Este programa calculará la distancia entre 2 puntos");
+
+                //Declaración de variables
+                double x1, x2, y1, y2, formula, restaX, restaY;
+
+                //Pedida de datos
+                Console.WriteLine("Introduzca el valor de 'X1'");
+                x1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Introduzca el valor de 'Y1'");
+                y1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Introduzca el valor de 'X2'");
+                x2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Introduzca el valor de 'Y2'");
+                y2 = double.Parse(Console.ReadLine());
+
+                restaX = x2 - x1;
+                restaY = y2 - y1;
+
+                //Creación de la fórmula
+                formula = Math.Pow(restaX, 2) + Math.Pow(restaY, 2);
+
+                //Impresion de resultado
+                Console.WriteLine($"La distancia entre el punto ({x1},{y1}) y ({x2},{y2}) es: {formula}");
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("--Tipo de dato ingresado no válido--");
+            }
+        }
+    }
+}
