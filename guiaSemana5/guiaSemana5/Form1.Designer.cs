3555e830ec7ccb506b783b8977ea7f9011c60228
Fernando Montano <fernando.montano41@gmail.com>
2021-02-13 17:59:18 -0600

    Subir la guia correspondiente de la semana

---
+++ guiaSemana5/guiaSemana5/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace guiaSemana5
+{
+    partial class formguia5
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formguia5));
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblsalario = new System.Windows.Forms.Label();
+            this.rbtn1 = new System.Windows.Forms.RadioButton();
+            this.rbtn2 = new System.Windows.Forms.RadioButton();
+            this.rbtn3 = new System.Windows.Forms.RadioButton();
+            this.lblejercicio = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.BackColor = System.Drawing.SystemColors.Control;
+            this.btncalcular.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(121, 331);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(199, 36);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = false;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(186, 119);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(160, 23);
+            this.txtnombre.TabIndex = 1;
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsalario.Location = new System.Drawing.Point(186, 166);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(160, 23);
+            this.txtsalario.TabIndex = 2;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.BackColor = System.Drawing.Color.Ivory;
+            this.lblnombre.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.ForeColor = System.Drawing.Color.Black;
+            this.lblnombre.Location = new System.Drawing.Point(101, 119);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(79, 19);
+            this.lblnombre.TabIndex = 3;
+            this.lblnombre.Text = "Nombre:";
+            // 
+            // lblsalario
+            // 
+            this.lblsalario.AutoSize = true;
+            this.lblsalario.BackColor = System.Drawing.Color.Ivory;
+            this.lblsalario.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsalario.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblsalario.Location = new System.Drawing.Point(101, 166);
+            this.lblsalario.Name = "lblsalario";
+            this.lblsalario.Size = new System.Drawing.Size(72, 19);
+            this.lblsalario.TabIndex = 4;
+            this.lblsalario.Text = "Salario:";
+            // 
+            // rbtn1
+            // 
+            this.rbtn1.AutoSize = true;
+            this.rbtn1.BackColor = System.Drawing.Color.Ivory;
+            this.rbtn1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn1.ForeColor = System.Drawing.Color.Black;
+            this.rbtn1.Location = new System.Drawing.Point(65, 253);
+            this.rbtn1.Name = "rbtn1";
+            this.rbtn1.Size = new System.Drawing.Size(86, 22);
+            this.rbtn1.TabIndex = 5;
+            this.rbtn1.TabStop = true;
+            this.rbtn1.Text = "Gerente";
+            this.rbtn1.UseVisualStyleBackColor = false;
+            // 
+            // rbtn2
+            // 
+            this.rbtn2.AutoSize = true;
+            this.rbtn2.BackColor = System.Drawing.Color.Ivory;
+            this.rbtn2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn2.ForeColor = System.Drawing.Color.Black;
+            this.rbtn2.Location = new System.Drawing.Point(167, 253);
+            this.rbtn2.Name = "rbtn2";
+            this.rbtn2.Size = new System.Drawing.Size(116, 22);
+            this.rbtn2.TabIndex = 6;
+            this.rbtn2.TabStop = true;
+            this.rbtn2.Text = "Sub gerente";
+            this.rbtn2.UseVisualStyleBackColor = false;
+            // 
+            // rbtn3
+            // 
+            this.rbtn3.AutoSize = true;
+            this.rbtn3.BackColor = System.Drawing.Color.Ivory;
+            this.rbtn3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn3.ForeColor = System.Drawing.Color.Black;
+            this.rbtn3.Location = new System.Drawing.Point(296, 253);
+            this.rbtn3.Name = "rbtn3";
+            this.rbtn3.Size = new System.Drawing.Size(105, 22);
+            this.rbtn3.TabIndex = 7;
+            this.rbtn3.TabStop = true;
+            this.rbtn3.Text = "Secretario";
+            this.rbtn3.UseVisualStyleBackColor = false;
+            // 
+            // lblejercicio
+            // 
+            this.lblejercicio.AutoSize = true;
+            this.lblejercicio.BackColor = System.Drawing.Color.Ivory;
+            this.lblejercicio.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblejercicio.ForeColor = System.Drawing.Color.Black;
+            this.lblejercicio.Location = new System.Drawing.Point(168, 65);
+            this.lblejercicio.Name = "lblejercicio";
+            this.lblejercicio.Size = new System.Drawing.Size(120, 25);
+            this.lblejercicio.TabIndex = 8;
+            this.lblejercicio.Text = "Ejercicio 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Ivory;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(138, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Selecciona tu cargo";
+            // 
+            // formguia5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(455, 443);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblejercicio);
+            this.Controls.Add(this.rbtn3);
+            this.Controls.Add(this.rbtn2);
+            this.Controls.Add(this.rbtn1);
+            this.Controls.Add(this.lblsalario);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btncalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formguia5";
+            this.Text = "Cálculo de descuento";
+            this.Load += new System.EventHandler(this.formguia5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lblsalario;
+        private System.Windows.Forms.RadioButton rbtn1;
+        private System.Windows.Forms.RadioButton rbtn2;
+        private System.Windows.Forms.RadioButton rbtn3;
+        private System.Windows.Forms.Label lblejercicio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ guiaSemana5/guiaSemana5/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guiaSemana5
+{
+    public partial class formguia5 : Form
+    {
+        public formguia5()
+        {
+            InitializeComponent();
+        }
+
+        private void formguia5_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombre;
+                double salario, descuento, salariodescuento;
+
+                nombre = txtnombre.Text;
+                salario = Convert.ToDouble(txtsalario.Text);
+
+                if (rbtn1.Checked == true)
+                {
+                    descuento = salario * 0.20;
+                    salariodescuento = salario - descuento;
+                    MessageBox.Show($"Bienvenido, {nombre}. " +
+                        $"\nTu puesto es: {rbtn1.Text}." +
+                        $"\nTu salario es de: ${salario}" +
+                        $"\nSe te descuenta: ${descuento}" +
+                        $"\nY tu salario luego de los descuentos es de: ${salariodescuento}");
+                }
+                if (rbtn2.Checked == true)
+                {
+                    descuento = salario * 0.15;
+                    salariodescuento = salario - descuento;
+                    MessageBox.Show($"Bienvenido, {nombre}. " +
+                        $"\nTu puesto es: {rbtn2.Text}." +
+                        $"\nTu salario es de: ${salario}" +
+                        $"\nSe te descuenta: ${descuento}" +
+                        $"\nY tu salario luego de los descuentos es de: ${salariodescuento}");
+                }
+                if (rbtn3.Checked == true)
+                {
+                    descuento = salario * 0.05;
+                    salariodescuento = salario - descuento;
+                    MessageBox.Show($"Bienvenido, {nombre}. " +
+                        $"\nTu puesto es: {rbtn3.Text}." +
+                        $"\nTu salario es de: ${salario}" +
+                        $"\nSe te descuenta: ${descuento}" +
+                        $"\nY tu salario luego de los descuentos es de: ${salariodescuento}");
+                }
+                if (rbtn1.Checked == false && rbtn2.Checked == false && rbtn3.Checked == false)
+                {
+                    MessageBox.Show("Debes marcar una casilla");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("No se aceptan cambios vacíos.");
+            }
+        }
+    }
+}
